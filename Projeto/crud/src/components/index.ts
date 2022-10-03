36a978aea2eb960c14d66c35f0637825eeb69759
DiasVitoria <vitoriadias384@gmail.com>
2022-10-03 20:52:23 -0300

    ligação da tela de crud com o projeto refatorado

--- Projeto/crud/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Aircrafts/aircrafts'
-export * from './Login/login'
-export * from './Menu/menu'
-export * from './Header/header'
--- Projeto/frontend/src/app/pages/Menu/menu.tsx
+++ Projeto/frontend/src/app/pages/Menu/menu.tsx
@@ -1,5 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalculator,
+  faPlane,
+} from "@fortawesome/free-solid-svg-icons";
+import "./menu.css";
+
 export const Menu = () => {
-    return(
-        <h1>Menu</h1>
-    )
-}
+  const history = useNavigate();
+
+  const handleCadastro = () => {
+    history("/aircraft");
+  };
+
+    const handleCalculo = () => {
+      history("/calc");
+    };
+
+  return (
+    <>
+      <div className="menu">
+        <div onClick={handleCadastro} className="menuButton">
+          <FontAwesomeIcon className="icon" icon={faPlane} />
+          <button>Cadastro</button>
+        </div>
+        <div onClick={handleCalculo} className="menuButton">
+          <FontAwesomeIcon className="icon" icon={faCalculator} />
+          <button>Cálculo</button>
+        </div>
+      </div>
+    </>
+  );
+};
